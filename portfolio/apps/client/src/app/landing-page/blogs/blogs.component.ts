3d690725c93b1e9ef416edffc9145f300d7d11e9
Prinju Koshy <44252825+prinjuk@users.noreply.github.com>
2021-08-08 15:46:50 +0530

    init nx

---
+++ portfolio/apps/client/src/app/landing-page/blogs/blogs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-blogs',
+  templateUrl: './blogs.component.html',
+  styleUrls: ['./blogs.component.scss']
+})
+export class BlogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ portfolio/apps/client/src/app/landing-page/landing-page.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page.component';
+import { BlogsComponent } from './blogs/blogs.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LandingPageComponent,
+    BlogsComponent
+  ],
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule
+  ]
+})
+export class LandingPageModule { }
