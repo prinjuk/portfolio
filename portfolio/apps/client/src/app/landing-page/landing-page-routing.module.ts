4e86309c19017065cd15efcef5ba9f67d3878af8
Prinju Koshy <44252825+prinjuk@users.noreply.github.com>
2021-08-08 20:12:42 +0530

    home pagestarted with assets

--- portfolio/apps/client/src/app/landing-page/landing-page-routing.module.ts
+++ portfolio/apps/client/src/app/landing-page/landing-page-routing.module.ts
@@ -2,20 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogsComponent } from './blogs/blogs.component';
 import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:HomeComponent
-  },
-  {
-    path:'blog',
-    component:BlogsComponent
+    path: '',
+    component: LandingPageComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'blog',
+        component: BlogsComponent,
+      },
+    ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LandingPageRoutingModule { }
+export class LandingPageRoutingModule {}
