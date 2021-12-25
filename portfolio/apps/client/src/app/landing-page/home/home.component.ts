7289295c8a4c5c5ef96d91f559187bd9acaf5239
Prinju Koshy <44252825+prinjuk@users.noreply.github.com>
2021-12-25 20:49:03 +0530

    new build

--- portfolio/apps/client/src/app/landing-page/home/home.component.ts
+++ portfolio/apps/client/src/app/landing-page/home/home.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 // import Swiper core and required modules
-import  { SwiperOptions  } from 'swiper';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'portfolio-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   config: SwiperOptions = {
     slidesPerView: 1,
     spaceBetween: 50,
-    navigation: false,
-    pagination: { type: 'fraction'}, 
+    pagination: true,
+    navigation: true,
+    grabCursor: true,
+    loop: true,
+    autoHeight: true,
+    autoplay: {
+      delay: 1500,
+    },
+    scrollbar: true,
   };
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
